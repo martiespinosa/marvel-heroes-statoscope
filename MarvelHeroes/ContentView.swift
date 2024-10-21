8f02ee92115dbfc04074bb79e574920c897d76cd
Martí Espinosa <martiespinosa99@gmail.com>
2024-10-21 09:23:35 +0200

    Initial Commit

---
+++ MarvelHeroes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarvelHeroes
+//
+//  Created by Martí Espinosa Farran on 21/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarvelHeroes/MarvelHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHeroesApp.swift
+//  MarvelHeroes
+//
+//  Created by Martí Espinosa Farran on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
